2b3e889b3c07bd0edc560662ab9d8a32715349b9
mshifflette <73154359+mshifflette@users.noreply.github.com>
2021-11-16 14:54:02 -0800

    adding env

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 const routes = require("./controllers")
 
-mongoose.connect('mongodb://localhost/workout');
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
